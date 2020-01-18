665ea9847bf2d7c962ffe140fdfa1800a8525804
joash <email@example.com>
2020-01-18 13:55:50 -0600

    added objects in controller

--- src/main/java/com/datapp/controllers/BookController.java
+++ src/main/java/com/datapp/controllers/BookController.java
@@ -1,13 +1,31 @@
 package com.datapp.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.datapp.entities.Book;
 @Controller
 public class BookController {
 	
-	@RequestMapping()
-	public String bookPage() {
-		return "bookpage";
+	@RequestMapping("/bookpage")
+	public String bookPage(ModelMap model) {
+		//create  a list of 7 object from book
+		List<Book> books = new ArrayList<Book>();
+		books.add(new Book("101","Java for dummies","Become a programmer overnight"));
+		books.add(new Book("102","C++ for newbies","Learning C++ for kids"));
+		books.add(new Book("103","Python for everybody","Learn python in one day"));
+		books.add(new Book("104","Spring boot study guide","learn spring boot if you can"));
+		books.add(new Book("105","JEE","Become a programmer overnight"));
+		books.add(new Book("106","Hadhoop study guide","Learn Data sciance "));
+		books.add(new Book("107","React for everybody","Become a React developer overnight"));
+		
+		model.put("books", books);
+		
+		return "bookpage.jsp";
 	}
 
 }
